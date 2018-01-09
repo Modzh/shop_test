3a8a9903bccc0af3e1c9e3920451e3b638d4f5b9
Modzh <modzh98@gmail.com>
2018-01-10 02:24:13 +0300

    updated setting param watchGood

--- src/main/java/app/servlets/sellGoodsServlet.java
+++ src/main/java/app/servlets/sellGoodsServlet.java
@@ -1,5 +1,9 @@
 package app.servlets;
 
+import app.entities.Good;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,10 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class sellGoodsServlet extends HttpServlet {
+private SessionFactory factory;
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        factory = (SessionFactory) SpringContextProvider.getContext().getBean("sessionFactory");
+    }
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("pages/sellGoods.jsp");
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("sellGoods.jsp");
         requestDispatcher.forward(req,resp);
     }
 
@@ -20,8 +31,18 @@ public class sellGoodsServlet extends HttpServlet {
         String nameGoods = req.getParameter("nameGoods");
         String short_desc = req.getParameter("short_desc");
         String description = req.getParameter("description");
-        int price = Integer.parseInt(req.getParameter("price"));
+        double price = Double.parseDouble(req.getParameter("price"));
+        String photo_address = "images/test_photo.jpg"; //TODO: getting photo address from req
+        int seller_id=0; //TODO: getting id of current user
+
+        Good good = new Good(seller_id, nameGoods, short_desc, description, price, photo_address);
 
 
+        Session session = factory.openSession();
+        session.beginTransaction();
+        session.saveOrUpdate(good);
+        session.getTransaction().commit();
+
+        doGet(req,resp);
     }
 }
